7132d4991b68063609fd5cddbefb1a8e9ac4d2e7
DEGUIL Matéo <deguil.mateo@gmail.com>
2025-03-26 09:49:46 +0100

    Ajout des menu dans le main

--- ApplicationMuscu/src/View/MainConsole.java
+++ ApplicationMuscu/src/View/MainConsole.java
@@ -1,10 +1,87 @@
 package View;
 
+import java.util.Scanner;
+
 public class MainConsole {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int choixMenuPrincipal = menuPrincipal();
+		boolean sortie = false;
+		while (sortie =! true) {
+			switch(choixMenuPrincipal) {
+			case 1 :
+				// Gestionnaire d'exercice
+				break;
+			case 2 : 
+				// Gestionnaire de Seance
+				break;
+			case 3 : 
+				// Gestionnaire de programme
+				break;
+			case 4 :
+				// Sortie du programme 
+				sortie = true;
+				break;
+			default :
+				System.out.println("Erreur !!");
+				break;
+			}
+		}
 
+	}
+	
+	public static int menuPrincipal() {
+		int choix = 0;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Bienvenu dans le Menu :");
+		System.out.println("	1 - Gestionnaire d'exercice.");
+		System.out.println("	2 - Gestionnaire de séance.");
+		System.out.println("	3 - Gestionnaire de programme.");
+		System.out.println("	4 - Quittez.");
+		System.out.println("Entrez votre choix");
+		choix = scan.nextInt();
+
+		return choix;
+	}
+	
+	public static int menuExercice() {
+		int choix;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Bienvenu dans le gestionnaire des exercices : ");
+		System.out.println("	1 - Créer un exercice.");
+		System.out.println("	2 - Modifier un exercice.");
+		System.out.println("	3 - Supprimer un exercice.");
+		System.out.println("	4 - Afficher un exercice.");
+		System.out.println("	5 - Quittez.");
+		System.out.println("Entrez votre choix");
+		choix = scan.nextInt();
+		return choix;
+	}
+	
+	public static int menuSeance() {
+		int choix;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Bienvenu dans le gestionnaire des seances : ");
+		System.out.println("	1 - Ajouter un exercice.");
+		System.out.println("	2 - Supprimer un exercice.");
+		System.out.println("	3 - Afficher la seance.");
+		System.out.println("	4 - Quittez.");
+		System.out.println("Entrez votre choix");
+		choix = scan.nextInt();
+		return choix;
+	}
+	
+	public static int menuProgramme() {
+		int choix;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Bienvenu dans le gestionnaire des seances : ");
+		System.out.println("	1 - Ajouter une seance.");
+		System.out.println("	2 - Supprimer une seance.");
+		System.out.println("	3 - Afficher le programe.");
+		System.out.println("	4 - Quittez.");
+		System.out.println("Entrez votre choix");
+		choix = scan.nextInt();
+		return choix;
 	}
 
 }
